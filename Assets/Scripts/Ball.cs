0ac7f152789332e9349f7d3afc53e5039bfc9c23
luisd159 <poteroa@uninorte.edu.co>
2022-04-22 10:04:03 -0500

    arreglando 2

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,9 +7,11 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rigbod;
     public int number;
     public bool potted;
+    public Collider2D col;
     // Start is called before the first frame update
     void Start()
     {
+        col = GetComponent<Collider2D>();
         rigbod = GetComponent<Rigidbody2D>();
     }
 
